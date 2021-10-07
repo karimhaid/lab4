d58a2d52798b530c215f0143caac0c119a0daebd
karim haidar <karimhai@umich.edu>
2021-10-07 09:46:00 -0400

    debugging

--- Hello.java
+++ Hello.java
@@ -2,8 +2,10 @@ public class Hello {
   public static void main(String[] args) {
     for(int i=1;i<=12;i++)
      {System.out.println("Hello World..."+i);}
-   }
+
    for (int i=1;i<=5;i++){
      System.out.println("additional,forgot to take screenshots");
    }
+
+  }
  }
